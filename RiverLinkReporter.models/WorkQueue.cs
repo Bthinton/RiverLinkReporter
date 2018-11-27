97e325a329224574586e5b859e8b07babf495475
Bthinton <bthinton101@gmail.com>
2018-11-27 13:49:03 -0500

    added workqueue model

---
+++ RiverLinkReporter.models/WorkQueue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiverLinkReporter.models
+{
+    public enum WorkQueueType
+    {
+        TestPassword,
+        GetData
+    }
+    //WorkQueue _ID, WorkQueueType (CheckPassword, GetData), User_ID, Complete (bit), CreatedDate, LastMOdifiedDate
+    public class WorkQueue
+    {
+        public int WorkQueue_id { get; set; }
+
+        public int User_id { get; set; }
+
+        public bool Completed { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime LastModifiedDate { get; set; }
+    }
+}
